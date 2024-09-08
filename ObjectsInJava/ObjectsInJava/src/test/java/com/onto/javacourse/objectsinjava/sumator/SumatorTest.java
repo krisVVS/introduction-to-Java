378ac0dbe56f322a7067dc61a213dcf2bb3fdf3d
vasilevkristian <kikovenelinov@gmail.com>
2024-09-08 23:27:28 +0300

    added KarelJ into the classpath

--- ObjectsInJava/ObjectsInJava/src/test/java/com/onto/javacourse/objectsinjava/sumator/SumatorTest.java
+++ ObjectsInJava/ObjectsInJava/src/test/java/com/onto/javacourse/objectsinjava/sumator/SumatorTest.java
@@ -4,43 +4,41 @@ import static org.junit.jupiter.api.Assertions.*;
 import java.math.BigDecimal;
 import java.math.BigInteger;
 
-public class SumatorTest {
+class SumatorTest {
 
     Sumator sumator = new Sumator();
 
     @Test
-    public void testSumIntegers() {
+    void testSumIntegers() {
         int result = sumator.sum(10, 20);
         assertEquals(30, result);
     }
 
     @Test
-    public void testSumDoubles() {
+    void testSumDoubles() {
         double result = sumator.sum(10.5, 20.3);
         assertEquals(30.8, result, 0.0001);
     }
 
     @Test
-    public void testSumStrings() {
+    void testSumStrings() {
         double result = sumator.sum("10.5", "20.3");
         assertEquals(30.8, result, 0.0001);
     }
 
     @Test
-    public void testSumStringsInvalidInput() {
-        assertThrows(NumberFormatException.class, () -> {
-            sumator.sum("abc", "20.3");
-        });
+    void testSumStringsInvalidInput() {
+        assertThrows(NumberFormatException.class, () -> sumator.sum("abc", "20.3"));
     }
 
     @Test
-    public void testSumBigIntegers() {
+    void testSumBigIntegers() {
         BigInteger result = sumator.sum(new BigInteger("1000000000000000000"), new BigInteger("2000000000000000000"));
         assertEquals(new BigInteger("3000000000000000000"), result);
     }
 
     @Test
-    public void testSumBigDecimals() {
+    void testSumBigDecimals() {
         BigDecimal result = sumator.sum(new BigDecimal("10.5"), new BigDecimal("20.3"));
         assertEquals(new BigDecimal("30.8"), result);
     }
