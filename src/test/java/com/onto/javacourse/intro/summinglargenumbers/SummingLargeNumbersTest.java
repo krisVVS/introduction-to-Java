567ec4811c979ea63b6aa943b9fede8c17c0b7c5
Kristian Vasilev <vasilevkrizztian@gmail.com>
2024-09-11 19:23:13 +0300

    made the input in two seperate parameters

--- src/test/java/com/onto/javacourse/intro/summinglargenumbers/SummingLargeNumbersTest.java
+++ src/test/java/com/onto/javacourse/intro/summinglargenumbers/SummingLargeNumbersTest.java
@@ -1,100 +1,110 @@
 package com.onto.javacourse.intro.summinglargenumbers;
-
 import org.junit.jupiter.api.Test;
-
 import static org.junit.jupiter.api.Assertions.*;
 
 class SummingLargeNumbersTest {
 
     @Test
     void testBasicNumbers() {
-        String input = "50 50";
+        String num1 = "50";
+        String num2 = "50";
         String expected = "100";
-        String actual = SummingLargeNumbers.summingLargeNumbers(input);
+        String actual = SummingLargeNumbers.summingLargeNumbers(num1, num2);
         assertEquals(expected, actual);
     }
 
     @Test
     void testNumbersWithLeadingZeros() {
-        String input = "00050 00050";
+        String num1 = "00050";
+        String num2 = "00050";
         String expected = "100";
-        String actual = SummingLargeNumbers.summingLargeNumbers(input);
+        String actual = SummingLargeNumbers.summingLargeNumbers(num1, num2);
         assertEquals(expected, actual);
     }
 
     @Test
     void testOneNumberWithLeadingZeros() {
-        String input = "100 005";
+        String num1 = "100";
+        String num2 = "005";
         String expected = "105";
-        String actual = SummingLargeNumbers.summingLargeNumbers(input);
+        String actual = SummingLargeNumbers.summingLargeNumbers(num1, num2);
         assertEquals(expected, actual);
     }
 
     @Test
     void testDifferentLengthsOfOutput() {
-        String input = "500 500";
+        String num1 = "500";
+        String num2 = "500";
         String expected = "1000";
-        String actual = SummingLargeNumbers.summingLargeNumbers(input);
+        String actual = SummingLargeNumbers.summingLargeNumbers(num1, num2);
         assertEquals(expected, actual);
     }
 
     @Test
     void testNumbersOfDifferentLengths() {
-        String input = "99999 1";
+        String num1 = "99999";
+        String num2 = "1";
         String expected = "100000";
-        String actual = SummingLargeNumbers.summingLargeNumbers(input);
+        String actual = SummingLargeNumbers.summingLargeNumbers(num1, num2);
         assertEquals(expected, actual);
     }
 
     @Test
     void testNumbersOfDifferentLengths2() {
-        String input = "0 100001";
+        String num1 = "0";
+        String num2 = "100001";
         String expected = "100001";
-        String actual = SummingLargeNumbers.summingLargeNumbers(input);
+        String actual = SummingLargeNumbers.summingLargeNumbers(num1, num2);
         assertEquals(expected, actual);
     }
 
     @Test
     void testInvalidInput() {
-        String input = "123 123!@#$123";
-        String actual = SummingLargeNumbers.summingLargeNumbers(input);
+        String num1 = "123";
+        String num2 = "123!@#$123";
+        String actual = SummingLargeNumbers.summingLargeNumbers(num1, num2);
         assertNull(actual);
     }
 
     @Test
     void testMoreThanTwoNumbersInvalidInput() {
-        String input = "123 456 789";
-        String actual = SummingLargeNumbers.summingLargeNumbers(input);
-        assertNull(actual);
+        String num1 = "123";
+        String num2 = "456";
+        String actual = SummingLargeNumbers.summingLargeNumbers(num1, num2);
+        assertNotNull(actual);
     }
 
     @Test
     void testNumberWithAllLeadingZeros() {
-        String input = "00000001 00000000";
+        String num1 = "00000001";
+        String num2 = "00000000";
         String expected = "1";
-        String actual = SummingLargeNumbers.summingLargeNumbers(input);
+        String actual = SummingLargeNumbers.summingLargeNumbers(num1, num2);
         assertEquals(expected, actual);
     }
 
     @Test
     void testEmptyInputInvalid() {
-        String input = "";
-        String actual = SummingLargeNumbers.summingLargeNumbers(input);
+        String num1 = "";
+        String num2 = "";
+        String actual = SummingLargeNumbers.summingLargeNumbers(num1, num2);
         assertNull(actual);
     }
 
     @Test
     void testArrayWithLengthOne() {
-        String input = "1234";
-        String actual = SummingLargeNumbers.summingLargeNumbers(input);
+        String num1 = "1234";
+        String num2 = "";
+        String actual = SummingLargeNumbers.summingLargeNumbers(num1, num2);
         assertNull(actual);
     }
 
     @Test
     void testLargeNumbers() {
-        String input = "5000000000 5000000000";
+        String num1 = "5000000000";
+        String num2 = "5000000000";
         String expected = "10000000000";
-        String actual = SummingLargeNumbers.summingLargeNumbers(input);
+        String actual = SummingLargeNumbers.summingLargeNumbers(num1, num2);
         assertEquals(expected, actual);
     }
 }
