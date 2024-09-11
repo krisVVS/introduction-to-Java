50d73f38bb69c0aa676f0f7b638a572be7cc101d
Kristian Vasilev <vasilevkrizztian@gmail.com>
2024-09-11 19:23:13 +0300

    made the input in two seperate parameters

--- src/main/java/com/onto/javacourse/intro/summinglargenumbers/SummingLargeNumbers.java
+++ src/main/java/com/onto/javacourse/intro/summinglargenumbers/SummingLargeNumbers.java
@@ -4,50 +4,52 @@ import org.slf4j.LoggerFactory;
 
 /**
  * Provides a method to sum two large numbers represented as strings.
- * The algorithm of reversing them and adding extra 0s until both of the numbers are the same length
- * After that we add every two one-digit numbers together + carry value which differs between 1 and 0
- * according if the sum of the two is more or less than 9
+ * The algorithm involves reversing them and adding extra 0s until both numbers are the same length.
+ * After that, we add every two one-digit numbers together + a carry value, which differs between 1 and 0
+ * depending on whether the sum of the two is greater than or equal to 10.
  */
 public class SummingLargeNumbers {
-
     private static final Logger log = LoggerFactory.getLogger(SummingLargeNumbers.class);
 
     /**
-     * Sums two large numbers provided as a single string.
+     * Sums two large numbers provided as two strings.
      *
-     * @param input a string containing two numbers separated by a space
+     * @param num1 the first large number as a string
+     * @param num2 the second large number as a string
      * @return the sum of the two numbers as a string, or null if the input is invalid
      */
-    public static String summingLargeNumbers(String input) {
+    public static String summingLargeNumbers(String num1, String num2) {
         StringBuilder result = new StringBuilder();
 
-        if (!isValidInput(input)) {
-            log.error("Wrong input: {}", input);
+        if (!isValidInput(num1) || !isValidInput(num2)) {
+            log.error("Invalid input: {} and {}", num1, num2);
             return null;
         }
 
-        String[] array = input.split(" ");
+        num1 = removeLeadingZeros(num1);
+        num2 = removeLeadingZeros(num2);
 
-        array[0] = removeLeadingZeros(array[0]);
-        array[1] = removeLeadingZeros(array[1]);
+        if (num1.length() < num2.length()) {
+            String temp = num1;
+            num1 = num2;
+            num2 = temp;
+        }
 
-        ensureFirstNumberIsLonger(array);
+        StringBuilder num1Builder = new StringBuilder(num1);
+        StringBuilder num2Builder = new StringBuilder(num2);
 
-        StringBuilder num1 = new StringBuilder(array[0]);
-        StringBuilder num2 = new StringBuilder(array[1]);
+        num1Builder.reverse();
+        num2Builder.reverse();
 
-        num1.reverse();
-        num2.reverse();
-
-        while (num2.length() < num1.length()) {
-            num2.append("0");
+        while (num2Builder.length() < num1Builder.length()) {
+            num2Builder.append("0");
         }
 
         int carry = 0;
 
-        for (int i = 0; i < num1.length(); i++) {
-            int digit1 = Character.getNumericValue(num1.charAt(i));
-            int digit2 = Character.getNumericValue(num2.charAt(i));
+        for (int i = 0; i < num1Builder.length(); i++) {
+            int digit1 = Character.getNumericValue(num1Builder.charAt(i));
+            int digit2 = Character.getNumericValue(num2Builder.charAt(i));
             int sum = digit1 + digit2 + carry;
 
             result.append(sum % 10);
@@ -62,15 +64,14 @@ public class SummingLargeNumbers {
     }
 
     /**
-     * Checks if the given string contains any letters.
+     * Checks if the given string contains only digits.
      *
      * @param str the string to check
-     * @return true if the string contains letters, false otherwise
+     * @return true if the string contains only digits, false otherwise
      */
     private static boolean isValidInput(String str) {
-        return str.matches("\\d+ \\d+");
+        return str.matches("\\d+");
     }
-
 
     /**
      * Removes leading zeros from a string representation of a number.
@@ -80,18 +81,5 @@ public class SummingLargeNumbers {
      */
     private static String removeLeadingZeros(String str) {
         return str.replaceFirst("^0+(?!$)", "");
-    }
-
-    /**
-     * Ensures the first number in the array is longer or equal in length to the second number.
-     *
-     * @param array an array containing two numbers as strings
-     */
-    private static void ensureFirstNumberIsLonger(String[] array) {
-        if (array[0].length() < array[1].length()) {
-            String temp = array[0];
-            array[0] = array[1];
-            array[1] = temp;
-        }
     }
 }
