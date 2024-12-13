e838244b1823cf4f55f425412346f75837299428
Kristian Vasilev <vasilevkrizztian@gmail.com>
2024-12-13 15:53:09 +0200

    task 3 + tests

--- Exception/src/main/java/com/onto/javacourse/exceptions/NumberOutOtRangeException.java
+++
@@ -1,7 +0,0 @@
-package com.onto.javacourse.exceptions;
-
-public class NumberOutOtRangeException extends Exception {
-    public NumberOutOtRangeException(String message) {
-        super(message);
-    }
-}
