28072a066fd17f762ede433040777091c4b17596
Kristian Vasilev <vasilevkristian@Kristians-MacBook-Pro.local>
2025-04-05 17:26:10 +0300

    fixed all the previous comments

--- Exception/src/main/java/com/onto/javacourse/between0n100/EmptyListException.java
+++ Exception/src/main/java/com/onto/javacourse/between0n100/EmptyListException.java
@@ -1,6 +1,8 @@
 package com.onto.javacourse.between0n100;
 
-/** Exception thrown when attempting to remove an element from an empty list. */
+/**
+ * Exception thrown when attempting to remove an element from an empty list.
+ */
 public class EmptyListException extends RuntimeException {
 
   /**
@@ -10,5 +12,15 @@ public class EmptyListException extends RuntimeException {
    */
   public EmptyListException(String message) {
     super(message);
+  }
+
+  /**
+   * Constructs a new EmptyListException with the specified detail message and cause.
+   *
+   * @param message the detail message explaining the cause of the exception
+   * @param cause the cause of the exception (a throwable that caused this exception to be thrown)
+   */
+  public EmptyListException(String message, Throwable cause) {
+    super(message, cause);
   }
 }
