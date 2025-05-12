9f3db26c3c9dca35814bf3d9972f189170b25c2d
Kristian Vasilev <vasilevkristian@Kristians-MacBook-Pro.local>
2025-05-13 02:29:40 +0300

    fixed the tests

--- Input-Output/src/test/java/com/onto/javacourse/inputoutput/consolereader/ConsoleReaderTest.java
+++ Input-Output/src/test/java/com/onto/javacourse/inputoutput/consolereader/ConsoleReaderTest.java
@@ -8,7 +8,7 @@ class ConsoleReaderTest {
 
   @Test
   void testReadString_ValidInput() {
-    String input = "hello\n";
+    String input = "hello"+System.lineSeparator();
     ConsoleReader reader = new ConsoleReader(new ByteArrayInputStream(input.getBytes()));
     String result = reader.readString();
     assertEquals("hello", result);
@@ -16,7 +16,7 @@ class ConsoleReaderTest {
 
   @Test
   void testReadInt_ValidInput() {
-    String input = "42\n";
+    String input = "42"+System.lineSeparator();
     ConsoleReader reader = new ConsoleReader(new ByteArrayInputStream(input.getBytes()));
     int result = reader.readInt();
     assertEquals(42, result);
@@ -24,7 +24,7 @@ class ConsoleReaderTest {
 
   @Test
   void testReadInt_InvalidThenValidInput() {
-    String input = "notANumber\n123\n";
+    String input = "notANumber"+System.lineSeparator()+"123"+System.lineSeparator();
     ConsoleReader reader = new ConsoleReader(new ByteArrayInputStream(input.getBytes()));
     int result = reader.readInt();
     assertEquals(123, result);
@@ -32,7 +32,7 @@ class ConsoleReaderTest {
 
   @Test
   void testReadChar_ValidInput() {
-    String input = "a\n";
+    String input = "a"+System.lineSeparator();
     ConsoleReader reader = new ConsoleReader(new ByteArrayInputStream(input.getBytes()));
     char result = reader.readChar();
     assertEquals('a', result);
@@ -40,7 +40,7 @@ class ConsoleReaderTest {
 
   @Test
   void testReadChar_InvalidThenValidInput() {
-    String input = "abc\nx\n";
+    String input = "abc"+System.lineSeparator()+"x"+System.lineSeparator();
     ConsoleReader reader = new ConsoleReader(new ByteArrayInputStream(input.getBytes()));
     char result = reader.readChar();
     assertEquals('x', result);
@@ -48,7 +48,7 @@ class ConsoleReaderTest {
 
   @Test
   void testReadFloat_ValidInput() {
-    String input = "3.14\n";
+    String input = "3.14"+System.lineSeparator();
     ConsoleReader reader = new ConsoleReader(new ByteArrayInputStream(input.getBytes()));
     float result = reader.readFloat();
     assertEquals(3.14f, result);
@@ -56,7 +56,7 @@ class ConsoleReaderTest {
 
   @Test
   void testReadFloat_InvalidThenValidInput() {
-    String input = "pi\n2.71\n";
+    String input = "pi"+System.lineSeparator()+"2.71"+System.lineSeparator();
     ConsoleReader reader = new ConsoleReader(new ByteArrayInputStream(input.getBytes()));
     float result = reader.readFloat();
     assertEquals(2.71f, result);
