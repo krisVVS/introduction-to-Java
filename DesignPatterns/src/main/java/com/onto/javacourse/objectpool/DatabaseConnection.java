6202f5806269b6aa570adce2cbcfc510581857a6
Kristian Vasilev <vasilevkristian@Kristians-MacBook-Pro.local>
2025-07-03 03:03:31 +0300

    added logger

--- DesignPatterns/src/main/java/com/onto/javacourse/objectpool/DatabaseConnection.java
+++ DesignPatterns/src/main/java/com/onto/javacourse/objectpool/DatabaseConnection.java
@@ -1,15 +1,18 @@
 package com.onto.javacourse.objectpool;
 
+import java.util.logging.Logger;
+
 /**
  * Represents a database connection that can be used for database operations.
  */
 public class DatabaseConnection {
+  Logger LOGGER = Logger.getLogger(DatabaseConnection.class.getName());
 
   /**
    * Constructs a new DatabaseConnection instance.
    * Prints a message when a new instance is created.
    */
   public DatabaseConnection() {
-    System.out.println("Creating new DatabaseConnection instance");
+    LOGGER.info("Database connection created");
   }
 }
